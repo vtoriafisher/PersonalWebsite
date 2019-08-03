ed38802d22ef2f94243911a9bc52e6885865a270
Victoria Fisher <50275233+vtoriafisher@users.noreply.github.com>
2019-08-03 16:14:12 -0400

    Update server.js

--- server.js
+++ server.js
@@ -49,6 +49,6 @@ app.use(function(err, req, res, next){
   res.render('500');
 });
 
-app.listen(app.get('port'), function(){
-  console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
